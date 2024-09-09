a4da89052ab4bde46d65d164c310a9958d027d88
leandrop22 <leandropetricca123@gmail.com>
2024-09-08 21:35:26 -0300

    Add files via upload

---
+++ petriccajpa/src/main/java/Entidades/Cliente.java
@@ -0,0 +1,57 @@
+package Entidades;
+
+import Entidades.*;
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+
+@Entity
+@Table(name = "Cliente")
+public class Cliente implements Serializable {
+
+    private static final long serialVersionID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "apellido")
+    private String apellido;
+
+    @Column(name = "dni", unique = true)
+    private int dni;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "fk_domicilio")
+    private Domicilio domicilio;
+
+    @OneToMany(mappedBy = "cliente")
+    private List<Factura> facturas = new ArrayList<>();
+
+    public Cliente(String apellido, int dni, Domicilio domicilio, String nombre) {
+        this.apellido = apellido;
+        this.dni = dni;
+        this.domicilio = domicilio;
+        this.nombre = nombre;
+    }
+
+    public Cliente(String apellido, int dni, String nombre) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+
+    }
+}
